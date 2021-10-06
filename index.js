ff5aa03bdd909fc257d115b8fdcffe2e97420981
ThristenJones <Thristenjones@yahoo.com>
2021-10-06 15:03:32 -0500

    added a delete request function to my index.js file

--- index.js
+++ index.js
@@ -30,4 +30,10 @@ app.put('/api/songs/:id', (req, res) => {
     return res.send(updatedSong)
 })
 
+app.delete('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.songs.deleteSong(id);
+    return res.send(updatedDataSet);
+});
+
 
