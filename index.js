8819d5c72b995213cc41aabf5ab593edb9a8f126
ThristenJones <Thristenjones@yahoo.com>
2021-10-06 14:21:13 -0500

    imported some middleware into index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const repoContext = require('./repository/repository-wrapper')
+
 
 app.listen(3000, function () {
     console.log("Server started")
 });
 
-app.get('/api/songs', (req, res)=> {
-    const music = repoContext.songs.findAllSongs();
-    return res.send(music);
+
+app.get('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
     
 });
 
