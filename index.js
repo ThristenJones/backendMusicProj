24b6c86cea1f4cc975c2851beddda0c2920f205c
ThristenJones <Thristenjones@yahoo.com>
2021-10-06 15:27:19 -0500

    imported middleware functionality into put amd post request functions on index.js file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const repoContext = require('./repository/repository-wrapper')
+const { validateProduct } = require('./middleware/products-validation');
 
 
 app.listen(3000, function () {
@@ -17,13 +18,13 @@ app.get('/api/songs/:id', (req, res) => {
     
 });
 
-app.post('/api/songs', (req, res) => {
+app.post('/api/songs', [validateProduct], (req, res) => {
     const newSong =req.body;
     const addedSong = repoContext.songs.createSong(newSong);
     return res.send(addedSong)
 })
 
-app.put('/api/songs/:id', (req, res) => {
+app.put('/api/songs/:id', [validateProduct], (req, res) => {
     const id = req.params.id;
     const songsPropertiesToUpdate = req.body;
     const updatedSong = repoContext.songs.updateSong(id,songsPropertiesToUpdate);
