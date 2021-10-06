912376edf0307478a1b47156bc95aa6d77a623a1
ThristenJones <Thristenjones@yahoo.com>
2021-10-05 21:44:04 -0500

    Added express, nodemon, and imported express into index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const repoContext = require('./repository/repository-wrapper')
 
 app.listen(3000, function () {
     console.log("Server started")
-})
+});
+
+app.get('/api/songs', (req, res)=> {
+    const music = repoContext.songs.findAllSongs();
+    return res.send(music);
+    
+});
+
+
